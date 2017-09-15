0025046aec2fea84ddf43fd5feda3b98eae1df3d
FranklinBasilio <basilius@ymail.com>
2017-09-15 20:19:44 -0300

    Aula Noite 15/09

---
+++ src/lp2/aula04/Cliente.java
@@ -0,0 +1,19 @@
+package lp2.aula04;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Cliente {
+    String nome,cpf;
+    
+    void mudarNome(String nome){
+        this.nome = nome;
+    }
+}
---
+++ src/lp2/aula04/ContaTeste.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lp2.aula04;
+
+/**
+ *
+ * @author usuario
+ */
+public class ContaTeste {
+    public static void main (String args[]){
+        Cliente c1 = new Cliente();
+        Agencia a = new Agencia();
+        
+        Conta conta1 = new Conta();
+        a.nome="Bradesco";
+        a.numero=384088;
+        conta1.numero=123;
+        conta1.limite=250;
+        conta1.saldo=100;
+        conta1.correntista=c1;
+        
+        conta1.correntista.nome="José da Silva";
+        conta1.correntista.cpf="07558694511";
+        
+        conta1.banco=a;
+        System.out.println("Cliente: "+conta1.saldo);
+        conta1.depositarValor(200);
+        System.out.println("Cliente: "+conta1.correntista.nome);
+        System.out.println("Cliente: "+conta1.saldo);
+        System.out.println(a.nome);
+        c1.mudarNome("Cayque");
+        System.out.println("Cliente: "+conta1.correntista.nome);
+        
+        System.out.println("Limite: "+conta1.consultarSaldo());
+    }
+}
